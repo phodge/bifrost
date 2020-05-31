ed760d7a4de47fa8ec449180f989538d4338ae24
Peter Hodge <peter.hodge84@gmail.com>
2020-06-01 06:33:33 +1000

    Helper to get a flask Blueprint quickly

--- bifrostrpc/__init__.py
+++ bifrostrpc/__init__.py
@@ -1,12 +1,18 @@
+import logging
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Tuple
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Tuple
 
 from typing_extensions import Literal
 
 from bifrostrpc.typing import Advanced, FuncSpec, Type
 from paradox.generate.files import FilePython, FileTS
 
+if TYPE_CHECKING:
+    import flask
+
 Flavour = Literal['requests', 'abstract']
+
+log = logging.getLogger()
 
 
 class ArgumentError(Exception):
@@ -105,3 +111,70 @@ class BifrostRPCService:
             adv=self._adv,
             flavour=flavour,
         )
+
+    def get_flask_blueprint(self, name: str, import_name: str) -> "flask.Blueprint":
+        from flask import Blueprint, Response
+
+        bp = Blueprint(name, import_name)
+
+        def _call(method: str) -> Response:
+            from flask import make_response, request
+            import json
+
+            # FIXME: provide a reuseable way to attach authentication/security
+            try:
+                fn, spec = self.getThings(method)
+
+                # GET request is allowed for methods with no arguments
+                if request.method == 'GET':
+                    # FIXME: apparently GET (and HEAD) are mandatory methods and must not return a
+                    # 405 error code like we're doing here.
+                    return make_response('Bifrost RPC method calls must be submitted by POST', 405)
+
+                provided = request.get_json()
+
+                # pop off the __showdataclass__ flag if it's present
+                showdataclasses = bool(provided.pop("__showdataclass__", False))
+
+                errors: List[str] = []
+                # import the data - this will type-check the whole thing and turn dicts into
+                # dataclasses as necessary, etc
+                kwargs = spec.importArgs(provided, 'body', errors)
+                if len(errors):
+                    return make_response('.\n'.join(errors) + '.', 400)
+
+                # now call the function
+                try:
+                    result: Any = fn(**kwargs)
+                except ArgumentError as e:
+                    errors = [e.args[0]]
+                else:
+                    # sanity-check the return value and convert fancy types (dataclasses) to plain
+                    # dicts
+                    errors = []
+                    jsonSafe = spec.exportRetval(result, '<retval>', showdataclasses, errors)
+
+                if len(errors):
+                    # TODO: in production mode we  need to log errors rather than sending them to
+                    # the client
+                    return make_response('.\n'.join(errors) + '.', 500)
+
+                # pack it up and send it back
+                # TODO: don't do pretty output in production mode
+                packed = json.dumps(jsonSafe, indent=2, sort_keys=True)
+                response = make_response(packed, 200)
+                response.headers['Content-Type'] = 'application/json'
+                return response
+            except InvalidMethodError:
+                return make_response(f'invalid method name {method!r}', 501)
+            except Exception as e:
+                # FIXME: in production mode we  need to log errors rather than sending them to the
+                # client
+                log.exception(f'BifrostRPC {name!r}: Exception encountered')
+                return make_response(str(e), 500)
+
+        bp.route('/api.v1/call/<method>', methods=['GET', 'POST'])(_call)
+
+        # FIXME: auto-generate a / route that lists the method calls and what they do
+
+        return bp
