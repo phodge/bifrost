306568946f3e82df967a977ee27db226ad668909
Peter Hodge <peter.hodge84@gmail.com>
2020-10-27 11:47:21 +1000

    Refactor failure mode classes into getFailureModeClasses()

--- bifrostrpc/generators/common.py
+++ bifrostrpc/generators/common.py
@@ -1,17 +1,25 @@
+from typing import Iterator, Tuple
+
 from paradox.generate.files import FileSpec
 from paradox.generate.statements import ClassSpec
 
 
 def appendFailureModeClasses(dest: FileSpec) -> None:
     dest.contents.remark('failure modes')
+    for name, classspec in getFailureModeClasses():
+        dest.contents.also(classspec)
+
+
+def getFailureModeClasses() -> Iterator[Tuple[str, ClassSpec]]:
     af = ClassSpec(
         'ApiFailure',
         docstring=['parent class of all failure modes'],
-        appendto=dest.contents,
         tsexport=True,
     )
     af.addProperty('message', str, initarg=True)
-    dest.contents.also(ClassSpec(
+    yield 'ApiFailure', af
+
+    yield 'ApiOutage', ClassSpec(
         'ApiOutage',
         bases=['ApiFailure'],
         tsbase='ApiFailure',
@@ -21,8 +29,9 @@ def appendFailureModeClasses(dest: FileSpec) -> None:
             'the method call succeeding. If a method fails with ApiOutage, you',
             'might possibly succeed on a retry.',
         ],
-    ))
-    dest.contents.also(ClassSpec(
+    )
+
+    yield 'ApiBroken', ClassSpec(
         'ApiBroken',
         bases=['ApiFailure'],
         tsbase='ApiFailure',
@@ -32,4 +41,4 @@ def appendFailureModeClasses(dest: FileSpec) -> None:
             'call from succeeding. It is unlikely you will get a successful',
             'result by retrying.',
         ]
-    ))
+    )
