f9d8598569d970e50e89c740bd5a16745149a869
Peter Hodge <peter.hodge84@gmail.com>
2022-10-23 22:16:34 +1000

    feature to generate PHP client

--- tests/test_client_generation.py
+++ tests/test_client_generation.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from os.path import dirname
 from pathlib import Path
 from subprocess import run
@@ -7,6 +8,54 @@ from textwrap import dedent
 from typing import Any
 
 import pytest
+
+
+def _run_php(script: str, **kwargs: Any) -> None:
+    cmd = ['php', '-d', 'assert.exception=1', script]
+    run(cmd, **kwargs, check=True)
+
+
+@pytest.mark.parametrize('flavour', ['abstract'])
+def test_generate_php_client(flavour: str, demo_service: Any, demo_service_port: int) -> None:
+    with TemporaryDirectory() as tmpdir:
+        clientpath = Path(tmpdir) / 'test_client.php'
+
+        if flavour == 'abstract':
+            demo_service.generatePHPClient(clientpath, 'TestClient', flavour='abstract')
+            demo_service_root = Path(__file__).parent / 'demo_service'
+            # install our get_client.php helper module
+            shutil.copy(
+                    demo_service_root / 'client_templates' / 'demo_curl_client.php',
+                    Path(tmpdir) / 'demo_curl_client.php'
+                    )
+        else:
+            raise Exception(f"Unexpected flavour {flavour!r}")
+
+        demo_script = dedent(
+            '''
+            <?php
+
+            require 'test_client.php';
+
+            require 'demo_curl_client.php';
+
+            $service = new DemoCurlClient();
+            $rev = $service->get_reversed("Hello world");
+            assert($rev === "dlrow olleH");
+            '''
+        )
+        demo_path = Path(tmpdir) / 'demo.php'
+        demo_path.write_text(demo_script)
+
+        run(
+            ['php', '-d', 'assert.exception=1', 'demo.php'],
+            check=True,
+            cwd=tmpdir,
+            env=dict(
+                **os.environ,
+                DEMO_SERVICE_PORT=str(demo_service_port),
+            ),
+        )
 
 
 @pytest.mark.parametrize('flavour', ['abstract', 'requests'])
