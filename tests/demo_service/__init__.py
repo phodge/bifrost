3cf8fe5396fbb7e09107d92cc13d8ebe7cc36f11
Peter Hodge <peter.hodge84@gmail.com>
2022-10-26 18:24:57 +1000

    Test generating RPC client for service that sends dataclass objects

--- tests/demo_service/__init__.py
+++ tests/demo_service/__init__.py
@@ -1,6 +1,9 @@
+from typing import List
+
 from flask import Flask
 
 from bifrostrpc import BifrostRPCService
+from tests.scenarios.pets import Pet
 
 service = BifrostRPCService()
 
@@ -10,11 +13,20 @@ class NoLogin:
 
 
 service.addAuthType(NoLogin, lambda: NoLogin())
+service.addDataclass(Pet)
 
 
 @service.rpcmethod
 def get_reversed(_: NoLogin, input_: str) -> str:
     return input_[::-1]
+
+
+@service.rpcmethod
+def get_pets(_: NoLogin) -> List[Pet]:
+    return [
+            Pet(name="Basil", species="dog", age=7),
+            Pet(name="Billy", species="dog", age=10),
+    ]
 
 
 app = Flask('tests.demo_service')
