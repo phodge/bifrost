87d15df48c1a6432b8d0e6b2cf12f84418b01b76
Peter Hodge <peter.hodge84@gmail.com>
2022-11-07 06:39:03 +1000

    Get more assertions working under Typescript

--- tests/scenarios/__init__.py
+++ tests/scenarios/__init__.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List, Protocol, Type, Union
 
-from paradox.expressions import PanExpr, Pannable, PanVar, pan
+from paradox.expressions import (NotSupportedError, PanExpr, Pannable, PanVar,
+                                 pan)
 from paradox.generate.statements import HardCodedStatement
 from paradox.interfaces import AcceptsStatements
 
@@ -48,20 +49,25 @@ class Scenario(Protocol):
 def assert_eq(context: AcceptsStatements, expr1: PanExpr, expr2: Union[PanExpr, Pannable]) -> None:
     if not isinstance(expr2, PanExpr):
         expr2 = pan(expr2)
+    context.alsoImportTS('./assertlib', ['assert_eq'])
     context.also(HardCodedStatement(
         python=f'assert ({expr1.getPyExpr()[0]}) == ({expr2.getPyExpr()[0]})',
         php=f'assert(({expr1.getPHPExpr()[0]}) === ({expr2.getPHPExpr()[0]}));',
+        typescript=f'assert_eq({expr1.getTSExpr()[0]}, {expr2.getTSExpr()[0]});',
     ))
 
 
 def assert_islist(context: AcceptsStatements, expr: PanExpr, *, size: int) -> None:
+    context.alsoImportTS('./assertlib', ['assert_islist'])
     context.also(HardCodedStatement(
         python=f'assert isinstance({expr.getPyExpr()[0]}, list)',
         php=f'assert(is_array({expr.getPHPExpr()[0]}));',
+        typescript=f'assert_islist({expr.getTSExpr()[0]}, {size});',
     ))
     context.also(HardCodedStatement(
         python=f'assert len({expr.getPyExpr()[0]}) == {size}',
         php=f'assert(count({expr.getPHPExpr()[0]}) === {size});',
+        typescript=None,
     ))
 
 
@@ -69,17 +75,21 @@ def assert_isdict(context: AcceptsStatements, expr: PanExpr, *, size: int) -> No
     context.also(HardCodedStatement(
         python=f'assert isinstance({expr.getPyExpr()[0]}, dict)',
         php=f'assert(is_array({expr.getPHPExpr()[0]}));',
+        typescript='throw new Error("TODO: FINISH assert_isdict");',
     ))
     context.also(HardCodedStatement(
         python=f'assert len({expr.getPyExpr()[0]}) == {size}',
         php=f'assert(count({expr.getPHPExpr()[0]}) === {size});',
+        typescript='throw new Error("TODO: FINISH assert_isdict");',
     ))
 
 
 def assert_isinstance(context: AcceptsStatements, expr: PanExpr, what: str) -> None:
+    # XXX: this isn't possible in TS because we're using structural typing
     context.also(HardCodedStatement(
         python=f'assert isinstance({expr.getPyExpr()[0]}, {what})',
         php=f'assert({expr.getPHPExpr()[0]} instanceof {what});',
+        typescript=f'// XXX: no easy way to prove that {expr.getTSExpr()[0]} is a {what}',
     ))
 
 
@@ -87,6 +97,7 @@ def assert_true(context: AcceptsStatements, expr: PanExpr) -> None:
     context.also(HardCodedStatement(
         python=f'assert {expr.getPyExpr()[0]}',
         php=f'assert({expr.getPHPExpr()[0]});',
+        typescript='throw new Error("TODO: FINISH assert_true");',
     ))
 
 
@@ -97,4 +108,5 @@ def assert_contains_text(context: AcceptsStatements, haystack: PanExpr, needle: 
     context.also(HardCodedStatement(
         python=f'assert re.search(r{needle!r}, {haystack.getPyExpr()[0]})',
         php=f'assert(strpos({haystackphp}, {needlephp}) !== false);',
+        typescript='throw new Error("TODO: FINISH assert_contains_text");',
     ))
