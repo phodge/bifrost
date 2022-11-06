163c5bcd7ec11836fd8fb51bb4b6b9f580b808ee
Peter Hodge <peter.hodge84@gmail.com>
2022-11-07 06:37:01 +1000

    Consolidate typescript test as well

--- tests/test_client_generation.py
+++ tests/test_client_generation.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+from typing import Union
 
 import pytest
-from paradox.expressions import PanCall, PanDict, pan
-from paradox.generate.statements import HardCodedStatement
+from paradox.expressions import (PanAwait, PanCall, PanDict, PanExpr, PanProp,
+                                 pan)
+from paradox.generate.statements import FunctionSpec, HardCodedStatement
+from paradox.interfaces import AcceptsStatements
 from paradox.output import Script
 from paradox.typing import CrossAny, CrossStr
 
@@ -18,86 +21,81 @@ DEMO_SERVICE_ROOT = Path(__file__).parent / 'demo_service'
 def test_generated_client(
     demo_runner: DemoRunner,
 ) -> None:
-    if demo_runner.lang not in ('php', 'python'):
-        return
-
     s = Script()
+
+    if demo_runner.lang == 'typescript':
+        if demo_runner.flavour == 'fetch':
+            # TODO: not yet implemented
+            return
+
+        # typescript requires await due to use of Promises
+        def await_call(
+            target: Union[str, PanProp],
+            *args: PanExpr,
+            **kwargs: PanExpr,
+        ) -> PanExpr:
+            return PanAwait(PanCall(target, *args, **kwargs))
+
+        # NOTE: we need to wrap the assertions in an async function because CommonJS format
+        # doesn't permit await at the top level
+        ctx: AcceptsStatements = s.also(FunctionSpec('test_body', 'no_return', isasync=True))
+    else:
+        # no need for await syntax in other languages
+        def await_call(
+            target: Union[str, PanProp],
+            *args: PanExpr,
+            **kwargs: PanExpr,
+        ) -> PanExpr:
+            return PanCall(target, *args, **kwargs)
+
+        ctx = s
 
     s.also(HardCodedStatement(
         php='require "get_client.php";',
         python=None,
+        typescript=None,
     ))
 
     s.remark('load the client')
     s.alsoImportPy('get_client', ['get_client'])
+    s.alsoImportTS('./get_client', ['get_client'])
     v_client = s.alsoDeclare('v_client', 'no_type', PanCall('get_client'))
 
-    s.remark('simple string-reversal endpoint')
+    ctx.remark('simple string-reversal endpoint')
     assert_eq(
-        s,
-        PanCall(v_client.getprop('get_reversed'), pan("Hello world")),
+        ctx,
+        await_call(v_client.getprop('get_reversed'), pan("Hello world")),
         "dlrow olleH",
     )
 
-    s.remark('method with a more complex return type')
-    v_pets = s.alsoDeclare('pets', CrossAny(), PanCall(v_client.getprop('get_pets')))
-    assert_islist(s, v_pets, size=2)
+    ctx.remark('method with a more complex return type')
+    v_pets = ctx.alsoDeclare('pets', CrossAny(), await_call(v_client.getprop('get_pets')))
+    assert_islist(ctx, v_pets, size=2)
     s.alsoImportPy('generated_client', ['Pet'])
-    assert_isinstance(s, v_pets.getindex(0), 'Pet')
-    assert_isinstance(s, v_pets.getindex(1), 'Pet')
-    assert_eq(s, v_pets.getindex(0).getprop('name'), 'Basil')
-    assert_eq(s, v_pets.getindex(1).getprop('name'), 'Billy')
+    assert_isinstance(ctx, v_pets.getindex(0), 'Pet')
+    assert_isinstance(ctx, v_pets.getindex(1), 'Pet')
+    assert_eq(ctx, v_pets.getindex(0).getprop('name'), 'Basil')
+    assert_eq(ctx, v_pets.getindex(1).getprop('name'), 'Billy')
 
-    s.remark('Testing a method that *receives* a complex argument type')
+    ctx.remark('Testing a method that *receives* a complex argument type')
     if demo_runner.lang == 'python':
-        s.remark("XXX: this is not yet supported in python")
-        s.remark("TODO: we can't pass dataclasses as args yet in python because python")
-        s.remark("doesn't automatically json-encode dataclasses")
+        ctx.remark("XXX: this is not yet supported in python")
+        ctx.remark("TODO: we can't pass dataclasses as args yet in python because python")
+        ctx.remark("doesn't automatically json-encode dataclasses")
     else:
         v_check_arg = PanDict({}, CrossStr(), CrossAny())
         v_check_arg.addPair(pan('basil'), v_pets.getindex(0))
         v_check_arg.addPair(pan('billy'), v_pets.getindex(1))
-        v_check = s.alsoDeclare('check', 'no_type', PanCall(
+        v_check = ctx.alsoDeclare('check', 'no_type', await_call(
             v_client.getprop('check_pets'),
             v_check_arg,
         ))
-        assert_eq(s, v_check, 'pets_ok!')
+        assert_eq(ctx, v_check, 'pets_ok!')
+
+    if ctx is not s:
+        s.also(PanCall('test_body'))
 
     demo_runner.run_demo(s)
-
-
-@pytest.mark.parametrize('flavour', ['abstract'])
-def test_generate_typescript_client(
-    flavour: str,
-    demo_service_port: int,
-    tmppath: Path,
-) -> None:
-    generate_demo_service_typescript_client(tmppath, flavour=flavour)
-
-    demo_script = '''
-        import {get_client} from './get_client';
-        import {Pet, ClientBase} from './generated_client';
-        import {ApiFailure} from './generated_client';
-        import {assert_eq, assert_islist} from './assertlib';
-
-        // NOTE: we need to wrap the assertions in an async function because CommonJS format
-        // doesn't permit await at the top level
-        (async () => {
-            let c: ClientBase = get_client();
-            const rev = await c.get_reversed("Hello world");
-            assert_eq(rev, "dlrow olleH");
-
-            const pets = await c.get_pets()
-            assert_islist(pets, 2);
-            assert_eq(pets[0].name, "Basil");
-            assert_eq(pets[1].name, "Billy");
-
-            const check = await c.check_pets({'basil': pets[0], 'billy': pets[1]})
-            assert_eq(check, 'pets_ok!');
-        })();
-        '''
-
-    run_typescript_demo(demo_script, root=tmppath, demo_service_port=demo_service_port)
 
 
 def test_generated_client_session_auth(demo_runner: DemoRunner) -> None:
