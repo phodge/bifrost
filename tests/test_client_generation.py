5ca283e274d0074af9c992ffd5fb598feeb88018
Peter Hodge <peter.hodge84@gmail.com>
2022-11-06 15:18:00 +1000

    Consolidate python-specific test into the generic unit test

--- tests/test_client_generation.py
+++ tests/test_client_generation.py
@@ -7,10 +7,8 @@ from paradox.output import Script
 from paradox.typing import CrossAny, CrossStr
 
 from tests.conftest import DemoRunner
-from tests.demo_service.execute import run_python_demo, run_typescript_demo
-from tests.demo_service.generate import (
-    generate_demo_service_python_client,
-    generate_demo_service_typescript_client)
+from tests.demo_service.execute import run_typescript_demo
+from tests.demo_service.generate import generate_demo_service_typescript_client
 from tests.scenarios import (assert_contains_text, assert_eq,
                              assert_isinstance, assert_islist)
 
@@ -20,7 +18,7 @@ DEMO_SERVICE_ROOT = Path(__file__).parent / 'demo_service'
 def test_generated_client(
     demo_runner: DemoRunner,
 ) -> None:
-    if demo_runner.lang != 'php':
+    if demo_runner.lang not in ('php', 'python'):
         return
 
     s = Script()
@@ -44,55 +42,28 @@ def test_generated_client(
     s.remark('method with a more complex return type')
     v_pets = s.alsoDeclare('pets', CrossAny(), PanCall(v_client.getprop('get_pets')))
     assert_islist(s, v_pets, size=2)
+    s.alsoImportPy('generated_client', ['Pet'])
     assert_isinstance(s, v_pets.getindex(0), 'Pet')
     assert_isinstance(s, v_pets.getindex(1), 'Pet')
     assert_eq(s, v_pets.getindex(0).getprop('name'), 'Basil')
     assert_eq(s, v_pets.getindex(1).getprop('name'), 'Billy')
 
     s.remark('Testing a method that *receives* a complex argument type')
-    v_check_arg = PanDict({}, CrossStr(), CrossAny())
-    v_check_arg.addPair(pan('basil'), v_pets.getindex(0))
-    v_check_arg.addPair(pan('billy'), v_pets.getindex(1))
-    v_check = s.alsoDeclare('check', 'no_type', PanCall(
-        v_client.getprop('check_pets'),
-        v_check_arg,
-    ))
-    assert_eq(s, v_check, 'pets_ok!')
+    if demo_runner.lang == 'python':
+        s.remark("XXX: this is not yet supported in python")
+        s.remark("TODO: we can't pass dataclasses as args yet in python because python")
+        s.remark("doesn't automatically json-encode dataclasses")
+    else:
+        v_check_arg = PanDict({}, CrossStr(), CrossAny())
+        v_check_arg.addPair(pan('basil'), v_pets.getindex(0))
+        v_check_arg.addPair(pan('billy'), v_pets.getindex(1))
+        v_check = s.alsoDeclare('check', 'no_type', PanCall(
+            v_client.getprop('check_pets'),
+            v_check_arg,
+        ))
+        assert_eq(s, v_check, 'pets_ok!')
 
     demo_runner.run_demo(s)
-
-
-@pytest.mark.parametrize('flavour', ['abstract', 'requests'])
-def test_generate_python_client(
-    flavour: str,
-    tmppath: Path,
-    demo_service_port: int,
-) -> None:
-    generate_demo_service_python_client(tmppath, flavour=flavour)
-
-    demo_script = '''
-        from get_client import get_client
-        from generated_client import Pet
-
-        t = get_client()
-        rev = t.get_reversed("Hello world")
-        assert rev == "dlrow olleH"
-
-        pets = t.get_pets()
-        assert isinstance(pets, list)
-        assert len(pets) == 2
-        assert isinstance(pets[0], Pet)
-        assert isinstance(pets[1], Pet)
-        assert pets[0].name == "Basil"
-        assert pets[1].name == "Billy"
-
-        # TODO: we can't pass dataclasses as args yet in python because python doesn't
-        # automatically json-encode dataclasses
-        # check = t.check_pets({'basil': pets[0], 'billy': pets[1]})
-        # assert check == 'pets_ok!'
-        '''
-
-    run_python_demo(demo_script, root=tmppath, demo_service_port=demo_service_port)
 
 
 @pytest.mark.parametrize('flavour', ['abstract'])
